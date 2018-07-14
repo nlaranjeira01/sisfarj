3e014ff71ca27eae3ad0dd1eb85e682b87e8d8c6
Paulo <Paulo@192.168.0.15>
2018-07-14 17:56:00 -0300

    ola

--- src/AddCredenciais.java
+++ src/AddCredenciais.java
@@ -7,7 +7,7 @@ import domain.AutenticacaoMT;
 import utils.StringAleatoria;
 
 public class AddCredenciais {
-
+//ola
 	public static void main(String[] args)
 	{
 		AutenticacaoMapper autM = new AutenticacaoMapper();
